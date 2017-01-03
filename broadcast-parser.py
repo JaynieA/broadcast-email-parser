063bebf16c68e86a2bdd50b5de7e863f1bb0e825
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-02 19:11:38 -0600

    connected to database to insert information, set up variable sleep times for continuous run

--- broadcast-parser.py
+++ broadcast-parser.py
@@ -4,6 +4,8 @@ import sys
 import re
 import time
 import email.header
+import pyodbc
+import datetime
 from email.utils import parseaddr
 from connection import *
 
@@ -14,23 +16,72 @@ def init():
     emailLogin(EMAIL_ACCOUNT, PASSWORD)
     selectMailbox(EMAIL_FOLDER)
 
+def deleteAllEmails(folderName):
+    #deletes all emails in the email folder passed in as a param
+    print('Deleting emails in %s...' % folderName)
+    M.select(folderName)
+    typ, data = M.search(None, 'ALL')
+    for num in data[0].split():
+        M.store(num, '+FLAGS', '\\Deleted')
+    M.expunge()
+    M.close()
+
+def determineSleepTime():
+    #Get current Time
+    now_time = datetime.datetime.now().time()
+    #Set start time at 6:00 PM
+    start_time = datetime.time(18, 0, 0)
+    #Set end time at 7:00 AM
+    end_time = datetime.time(7, 0, 0)
+    #If it is after 6:00PM, or before 7:00AM
+    if (now_time >= start_time or now_time <= end_time):
+        print('Sleeping for 3 hours...')
+        return 10800
+    else:
+        print('sleeping for 1 hour...')
+        return 3600
+
 def emailLogin(account, password):
     #Login to gmail account
     try:
         rv, data = M.login(account, password)
     #Display error message if login fails
     except imaplib.IMAP4.error:
-        print('Login Failed')
+        currentTime = datetime.datetime.now().strftime('%I:%M %p')
+        print('Login Failed at %s' % currentTime)
         #Exit to deal with login failure
         sys.exit(1)
     #display success info on login
     print(rv, data)
+    currentTime = datetime.datetime.now().strftime('%I:%M %p')
+    print('Logged in at %s' % currentTime)
 
 def getCompanyName(line):
     try:
         return line.split(':')[1].strip()
     except IndexError:
         return ''
+
+def getCompanyShortName(company_name):
+    #connect to the database
+    connection = pyodbc.connect(connStr)
+    try:
+        #Get company short name from database using company name in the email
+        with connection.cursor() as cursor:
+            sql = """SELECT Company.CompanyNameShort FROM Company WHERE (((Company.BbinName) = '"""+ company_name +"""'));"""
+            cursor.execute(sql)
+            #Get results from the query for company_name_short
+            company_name_short = [x[0] for x in cursor.fetchall()]
+            #Set to misc_reseller as default if company_name not in database
+            if company_name_short == None or company_name_short == []:
+                company_name_short = 'Misc_Reseller'
+            #If company_name is in the database, define value as company_name_short
+            else:
+                company_name_short = company_name_short[0]
+    finally:
+        #close the connection to the database
+        connection.close()
+    return company_name_short
 
 def getDateTimeInfo(body):
     #Use regex to get the date/time the original message was sent
@@ -72,10 +123,12 @@ def getMessages():
         #Initialize msg as raw email message
         msg = email.message_from_bytes(data[0][1])
         #Display current email count, get data from emails
-        print('\nEmail #%s' % email_count)
+        print('Email #%s' % email_count)
         parseMessage(msg, data)
         #Increment email counter
         email_count += 1
+        #Print dividing line for clarity
+        print('\n-----------------------Email End-----------------------\n')
 
 def getPersonName(line):
     regex = r'^.*?(?= P:)'
@@ -108,26 +161,45 @@ def parseMessage(msg, data):
     sendDateTime = getDateTimeInfo(body)
     sendDate = sendDateTime[0]
     sendTime = sendDateTime[1]
-    print('Date Sent: %s\nTime Sent: %s\n' % (sendDate, sendTime))
+    print('Date Sent: %s\nTime Sent: %s' % (sendDate, sendTime))
     #Separate search info from the rest of the body
     searchInfo = getSearchInfo(body)
-
-    #TODO: extract the following into it's own function (getDataFromSearches)
     #Get individual part searches to parse through
     partSearches = splitPartSearches(searchInfo)
     #For each part searched...
     for partSearch in partSearches:
         #Get the part number for the part searched
         part_num = getPart(partSearch)
-        print(part_num)
+        #Display the part number currently bein parsed
+        print('\nPart: %s' % part_num)
+        print('Searches:')
         #Get a split list of each individual search done
         eachSearch = getIndividualSearchesList(partSearch)
-
-        #TODO: make sure query is added to below function (and list) before moving on
         #Parse through each search by line and return a list of lists of results
-        #Format of results lists is: [part_searched, company_name, person_first_name, person_last_name]
+        #Format of results lists is: [part_searched, company_name, person_first_name, person_last_name, company_name_short]
         completeSearchInfoList = parseIndividualSearches(eachSearch)
-        print(completeSearchInfoList)
+        #Insert a record into the database
+        createRecord(completeSearchInfoList, part_num, sendDate, sendTime)
+
+def createRecord(infoList, part, date, time):
+    for search in infoList:
+        part_searched = search[0]
+        company_name = search[1]
+        person_first_name = search[2]
+        person_last_name = search[3]
+        company_name_short = search[4]
+        print (part_searched, company_name, person_first_name, person_last_name, company_name_short)
+        #connect to the database:
+        connection = pyodbc.connect(connStr)
+        try:
+            #insert record
+            with connection.cursor() as cursor:
+                sql = 'INSERT INTO tbl_broadcast_data (part_number, part_searched, search_date, search_time, company_name, company_name_short, person_first_name, person_last_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+                cursor.execute(sql, (part, part_searched, date, time, company_name, company_name_short, person_first_name, person_last_name))
+        except pyodbc.IntegrityError:
+            print ("INTEGRITY ERROR")
+        finally:
+            connection.close()
 
 def parseIndividualSearches(eachSearch):
     #Parse through eachSearch by line
@@ -139,8 +211,8 @@ def parseIndividualSearches(eachSearch):
         if (lineCounter % 2 != 0):
             part_searched = getPartSearched(line)
             company_name = getCompanyName(line)
-            #TODO: get company short name (or Misc_Reseller based on company_name from db)
-            #TODO: add the above to the list appended to completeSearchInfo and returned
+            #connect to the database to get company short name
+            company_name_short = getCompanyShortName(company_name)
         #Else, if the number is even (second line of individual search),
         #get the name of the person
         elif (lineCounter % 2 == 0):
@@ -155,7 +227,7 @@ def parseIndividualSearches(eachSearch):
                 person_last_name = ''
         #Print all info after search has been parsed (every 2 lines)
         if (lineCounter % 2 == 0):
-            completeSearchInfo.append([part_searched, company_name, person_first_name, person_last_name])
+            completeSearchInfo.append([part_searched, company_name, person_first_name, person_last_name, company_name_short])
         #Increment the lineCounter
         lineCounter += 1
     return completeSearchInfo
@@ -164,7 +236,7 @@ def selectMailbox(folder):
     #Select mailbox folder, mark new messages as read
     rv, data = M.select(folder, readonly = False)
     if rv == 'OK':
-        print('Processing Emails in %s \n' % folder)
+        print('Processing Emails in %s...\n' % folder)
         #Get all emails in the folder
         getMessages()
         #Close currently selected mailbox
@@ -178,4 +250,17 @@ def splitPartSearches(searchList):
     return searchList.split('\r\n\r\n')
 
 #initialize app
-init()
+while True:
+    print('App Initialized... \n')
+    #Initialize login and parsing
+    init()
+    #delete all emails in Inbox
+    deleteAllEmails('Inbox')
+    #delete all emails that were parsed in EMAIL_FOLDER mailbox
+    deleteAllEmails(EMAIL_FOLDER)
+    #Log out of email account
+    M.logout()
+    #re-initialize class to allow reconnect on next login
+    M = imaplib.IMAP4_SSL('imap.gmail.com')
+    #Sleep for 1 hour or 3 hours, depending on current time
+    time.sleep(determineSleepTime())
